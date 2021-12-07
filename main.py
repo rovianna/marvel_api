690666e17bd374ec2c809b5fdcacee089d796afb
Rodrigo Vianna <rodrigogvianna@outlook.com>
2021-12-07 20:47:34 -0300

    Main file, dataframe adjustments

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-import hashlib
-import time
-import requests
+from network import requestMarvelAPI
+from character import defineCharacterFields
+import pandas as pd
 
-m = hashlib.md5()
-base_url = "https://gateway.marvel.com"
+def main():
+    characters_arr = []
+    for characters in requestMarvelAPI():
+        characters_arr.append(defineCharacterFields(characters))
+        dataframe = pd.DataFrame(characters_arr)
+    dataframe.to_csv('characters_dataframe.csv', encoding='utf-8')
+    print("I am... inevitable...")
+    print("=== DATAFRAME ===")
+    print(dataframe)
 
-query = "/v1/public/characters" + "?"
-
-ts = str(time.time())
-ts_byte = bytes(ts, 'utf-8')
-
-m.update(ts_byte)
-
-public_key = '7d25dee031cf5f3b0d95796877799204'
-private_key = '6454306651e49dc8ac6976e556839e94092af934'
-
-m.update(b"6454306651e49dc8ac6976e556839e94092af934")
-m.update(b"7d25dee031cf5f3b0d95796877799204")
-
-hasht = m.hexdigest()
-
-api_url = base_url + query + "ts=" + ts + "&apikey=" + public_key + "&hash=" + hasht
-print(api_url)
-
-data = requests.get(api_url).json()
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Marvel API
+
+## Libraries used
+- dotenv
+- requests
+- pandas
+
+### author: rodrigogvianna@outlook.com
+
